d66f251426d05cb2c847e9b5db0856aee22e75cb
unknown <ryancare@protonmail.com>
2022-06-11 09:14:16 -0500

    Fixed stateful logic for board parsing function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import './App.css';
 import {useState, useEffect} from 'react'
 
-const emptyBoard = Array.from({length: 3}, v => Array.from({length: 3}))
+const emptyBoard = Array.from({length: 9}, (v,i) => i);
 
 function App() {
 
-  const [board, setBoard] = useState(Array.from({length: 9}, (v,i) => i))
+  const [board, setBoard] = useState(emptyBoard);
   const [userTurn, setUserTurn] = useState(Math.random() < .5 ? true : false);
   const [sets, setSets] = useState([])
 
   const parseBoard = currentBoard => {
-    const horizontal = Array.from({length: 3}, v => Array.from({length: 3}));
-    const vertical = Array.from({length: 3}, v => Array.from({length: 3}));
-    const diagonal = Array.from({length: 2}, v => Array.from({length: 3}));
+    let horizontal = Array.from({length: 3}, v => []);
+    let vertical = Array.from({length: 3}, v => []);
+    let diagonal = Array.from({length: 2}, v => []);
+    console.log("horizontal", horizontal, "vertical", vertical, "diagonal", diagonal)
     currentBoard.forEach( (v,i) => {
-      if (i % 3 === 0) vertical[Math.floor(i % 3)].push(v);
+      vertical[i % 3 === 0 ? 0 : i % 3 === 1 ? 2 : 1].push(v);      
       horizontal[Math.floor(i / 3)].push(v);
       if (i % 4 === 0) diagonal[0].push(v);
       if (i % 2 === 0 && i > 0 && i < 8) diagonal[1].push(v);
+      console.log("horizontal", horizontal, "vertical", vertical, "diagonal", diagonal)
     })
     setSets([...vertical, ...horizontal, ...diagonal]);
   }
