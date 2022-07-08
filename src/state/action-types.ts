d9ff61793638540cfb9d5249e70bf6f22b86a401
unknown <ryancare@protonmail.com>
2022-07-08 17:31:12 -0400

    Removed now-fully-obviated action-types file

--- src/state/action-types.ts
+++
@@ -1,6 +0,0 @@
-export const SET_MESSAGE = "SET_MESSAGE";
-export const SET_BOARD = "SET_BOARD";
-export const SWITCH_TURNS = "SWITCH_TURNS";
-export const SET_WINNER = "SET_WINNER";
-export const SET_SCORE = "SET_SCORE";
-export const RESET_BOARD = "RESET_BOARD";
